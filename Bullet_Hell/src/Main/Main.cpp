bc5b55c4dbdee3ff5dfaf600cf7bac41eca43ebf
David Burks <chesburks@gmail.com>
2023-09-07 01:00:10 -0400

    Convert project away from make, add entry point

---
+++ Bullet_Hell/src/Main/Main.cpp
@@ -0,0 +1,14 @@
+#include "Main.h"
+
+#include <windows.h>
+#include <iostream>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+    return 0;
+}
+
+int main() {
+    std::cout << "Hello world!" << std::endl;
+    return 0;
+}
