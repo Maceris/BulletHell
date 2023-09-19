ec2e1cf534514be8cda57c0402e3e593c4697d99
David Burks <chesburks@gmail.com>
2023-09-18 22:59:54 -0400

    Fill out Gui Mesh

--- Bullet_Hell/src/Graphics/Graph/GuiMesh.cpp
+++ Bullet_Hell/src/Graphics/Graph/GuiMesh.cpp
@@ -5,6 +5,9 @@
 #include "imgui_impl_opengl3.h"
 
 GuiMesh::GuiMesh()
+	: indices_VBO(0)
+	, VAO(0)
+	, vertices_VBO(0)
 {
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -12,5 +15,23 @@ GuiMesh::GuiMesh()
 	glGenBuffers(1, &vertices_VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, vertices_VBO);
 	glEnableVertexAttribArray(0);
-	//glVertexAttribPointer(0, 2, GL_FLOAT, false, ImDrawData)
+	const int sizeof_draw_vert = sizeof(ImDrawVert);
+	glVertexAttribPointer(0, 2, GL_FLOAT, false, sizeof_draw_vert, 0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof_draw_vert, 
+		(const void*) 8);
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 4, GL_UNSIGNED_BYTE, false, sizeof_draw_vert, 
+		(const void*) 16);
+
+	glGenBuffers(1, &indices_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+GuiMesh::~GuiMesh()
+{
+	glDeleteBuffers(1, &indices_VBO);
+	glDeleteBuffers(1, &vertices_VBO);
+	glDeleteVertexArrays(1, &VAO);
 }
