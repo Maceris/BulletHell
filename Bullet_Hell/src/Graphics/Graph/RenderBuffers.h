a18eec4ff5b35daf411bc338d41c685fbf7f0796
David Burks <chesburks@gmail.com>
2023-09-23 00:15:22 -0400

    Update docs

--- Bullet_Hell/src/Graphics/Graph/RenderBuffers.h
+++ Bullet_Hell/src/Graphics/Graph/RenderBuffers.h
@@ -3,62 +3,82 @@
 #include "Globals.h"
 
 #include "glad.h"
-#define GLFW_INCLUDE_NONE
 #include "glfw3.h"
 
 #include <vector>
 
+#include "MeshDrawData.h"
 #include "Scene.h"
 
 class RenderBuffers
 {
 public:
-	// Prepares to be set up, but models must be loaded before use
+	
+	/// <summary>
+	/// Prepares to be set up, but models must be loaded before use.
+	/// </summary>
 	RenderBuffers();
 	
-	// Performs cleanup
+	/// <summary>
+	/// Clean up data.
+	/// </summary>
 	~RenderBuffers();
 
-	// Clean up all the buffers and data
+	/// <summary>
+	/// Clean up all the buffers and data. Should be called every time we need
+	/// to reload the scene data into the buffers.
+	/// </summary>
 	void cleanup();
 
-	/*
-	Load models with animation data in a scene. Make sure to clear out the
-	existing buffer data before doing this a second time.
-	*/
+	/// <summary>
+	/// Load models with animation data in a scene. Make sure to clear out the
+	/// existing buffer data before doing this a second time.
+	/// </summary>
+	/// <param name="scene">The scene to load data from.</param>
 	void load_animated_models(const Scene& scene);
 
-	/*
-	Load models with no animation data in a scene. Make sure to clear out the
-	existing buffer data before doing this a second time.
-	*/
+	/// <summary>
+	/// Load models with no animation data in a scene. Make sure to clear out
+	/// the existing buffer data before doing this a second time.
+	/// </summary>
+	/// <param name="scene">The scene to load data from.</param>
 	void load_static_models(const Scene& scene);
 
 private:
-	// The Vertex Array Object ID for animated models
+	/// <summary>
+	/// The Vertex Array Object ID for animated models.
+	/// </summary>
 	GLuint animated_VAO;
 	
-	// The Vertex Buffer Object ID for binding poses of animated models
+	/// <summary>
+	/// The Vertex Buffer Object ID for binding poses of animated models.
+	/// </summary>
 	GLuint binding_poses_VBO;
 
-	/*
-	The Vertex Buffer Object ID for indices and weights of the bones of
-	animated models
-	*/
+	/// <summary>
+	/// The Vertex Buffer Object ID for indices and weights of the bones of
+	/// animated models.
+	/// </summary>
 	GLuint bones_indices_weights_VBO;
 
-	// The Vertex Buffer Object ID for bones matrices of animated models
+	/// <summary>
+	/// The Vertex Buffer Object ID for bones matrices of animated models.
+	/// </summary>
 	GLuint bones_matrices_VBO;
 
-	/*
-	The Vertex Buffer Object ID for the transformed animation vertices
-	after processing of animated models
-	*/
+	/// <summary>
+	/// The Vertex Buffer Object ID for the transformed animation vertices
+	/// after processing of animated models.
+	/// </summary>
 	GLuint dest_animation_VBO;
 
-	// The Vertex Array Object ID for static models
+	/// <summary>
+	/// The Vertex Array Object ID for static models.
+	/// </summary>
 	GLuint static_VAO;
 
-	// A list of all the VBOs that have been set up
+	/// <summary>
+	/// A list of all the VBOs that have been set up.
+	/// </summary>
 	std::vector<GLuint> VBO_list;
 };
