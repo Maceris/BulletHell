2285aa42a807cff06dac9f304b65dbf868472ced
David Burks <chesburks@gmail.com>
2023-09-28 18:32:29 -0400

    Add spot light

--- Bullet_Hell/src/Graphics/Scene/Lights/SpotLight.cpp
+++ Bullet_Hell/src/Graphics/Scene/Lights/SpotLight.cpp
@@ -1,1 +1,25 @@
 #include "SpotLight.h"
+
+#include <cmath>
+#include <numbers>
+#define degreesToRadians(angleInDegrees) ((angleInDegrees) * std::numbers::pi / 180.0)
+
+SpotLight::SpotLight(const PointLight& point_light, const glm::vec3& cone_direction,
+	float cut_off_degrees)
+	: point_light(point_light)
+	, cone_direction(cone_direction)
+{
+	set_cut_off_angle(cut_off_degrees);
+}
+
+void SpotLight::set_cone_direction(const float x, const float y, const float z)
+{
+	cone_direction.x = x;
+	cone_direction.y = y;
+	cone_direction.z = z;
+}
+
+void SpotLight::set_cut_off_angle(const float cut_off_degrees)
+{
+	cut_off = std::cos(degreesToRadians(cut_off_degrees));
+}
--- Bullet_Hell/src/Graphics/Scene/Lights/SpotLight.h
+++ Bullet_Hell/src/Graphics/Scene/Lights/SpotLight.h
@@ -2,6 +2,55 @@
 
 #include "Globals.h"
 
-class SpotLight
+#include "glm.hpp"
+
+#include "PointLight.h"
+
+/// <summary>
+/// A light that shines in a cone.
+/// </summary>
+struct SpotLight
 {
+	/// <summary>
+	/// The direction the light is pointing towards.
+	/// </summary>
+	glm::vec3 cone_direction;
+
+	/// <summary>
+	/// The angle where we cut the light off, stored as cos(cut-off angle in 
+	/// radians).
+	/// </summary>
+	float cut_off;
+
+	/// <summary>
+	/// The light we are using, which is cut off everywhere except the cone.
+	/// </summary>
+	PointLight point_light;
+
+	/// <summary>
+	/// Set up a spot light.
+	/// </summary>
+	/// <param name="point_light">The point light that describes the lighting
+	/// properties.</param>
+	/// <param name="cone_direction">The direction the light is facing.</param>
+	/// <param name="cut_off_degrees">The angle in degrees away from the
+	/// directionthat we cut off lighte light at.</param>
+	SpotLight(const PointLight& point_light, const glm::vec3& cone_direction,
+		float cut_off_degrees);
+
+	/// <summary>
+	/// Set the direction that the light is shining in. This is the x, y, and
+	/// z coordinates of a vector that represent the direction of the light.
+	/// </summary>
+	/// <param name="r">The x component.</param>
+	/// <param name="g">The y component.</param>
+	/// <param name="b">The z component.</param>
+	void set_cone_direction(const float x, const float y, const float z);
+
+	/// <summary>
+	/// Set the cutoff angle, in degrees. Converted to
+	/// cos(toRadians(angle in degrees)).
+	/// </summary>
+	/// <param name="cut_off_degrees">The cut off angle in degrees.</param>
+	void set_cut_off_angle(const float cut_off_degrees);
 };
